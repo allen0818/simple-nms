4e78f915cc1ffd967930f765b64dc794c7fa9442
allen0818 <chshman310222@gmail.com>
2021-05-24 23:45:55 +0800

    rename the api project to nms_api

--- api/posts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    id = models.AutoField(auto_created=True, primary_key=True)
-    title = models.CharField(max_length=50, blank=True, default='')
-    content = models.TextField(blank=True, null=True, default='')
